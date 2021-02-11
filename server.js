fd04e759eeeeb3d4f195f533f49ebb3a93d7244d
wdfhai <wd.fhai@outlook.com>
2021-02-11 12:23:25 -0600

    Initial Code base

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
