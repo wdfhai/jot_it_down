935fad72abdea717bbed7f24efc56c9597c008d6
wdfhai <wd.fhai@outlook.com>
2021-02-11 23:20:55 -0600

    added unique id generating module

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { uniqueNamesGenerator, NumberDictionary } = require('unique-names-generator');
+const notes = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,27 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(__dirname + "/public"));
 
-const notes = [];
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-
-    newNote.id = newNote.title.replace(/\s+/g, '').toLowerCase();
-
+    newNote.id = parseInt(NumberDictionary.generate({ min: 1000, max: 9999 }));
     notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(newNote);
 });
-
-
-
-
-
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
