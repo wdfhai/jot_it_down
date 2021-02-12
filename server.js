b2aa603b5f5799cc83e3bdfd9a9989915157beee
wdfhai <wd.fhai@outlook.com>
2021-02-11 20:39:20 -0600

    fixed saveNote functionality

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/public", express.static(__dirname + "/public"));
 
+const notes = [];
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+app.get('/api/notes', (req, res) => res.json(notes));
 
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
 
+    newNote.id = newNote.title.replace(/\s+/g, '').toLowerCase();
 
-
-
-
-
-
-
-
-
+    notes.push(newNote);
+    res.json(newNote);
+});
 
 
 
